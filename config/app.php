36e2f710bee43d02357145d49b60763c33e8865a
Andrew Sam <i4real@zoho.com>
2019-12-10 18:00:44 +0000

    Added Laravel cors package

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Felixkiss\UniqueWithValidator\ServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
     ],
 
     /*
