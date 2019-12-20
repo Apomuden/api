13df36fffea98cdf3cbbf12697f41b2eee65d8f0
Andrew Sam <i4real@zoho.com>
2019-12-20 17:41:51 +0000

    Added Post Routes for Id Types,Educational Levels and Age Groups

--- app/Http/Requests/Setups/AgeGroupRequest.php
+++ app/Http/Requests/Setups/AgeGroupRequest.php
@@ -14,7 +14,7 @@ class AgeGroupRequest extends ApiFormRequest
             'name' => 'bail|'.($id?'sometimes':'required').'|string|unique_with:departments,district_id'.(isset($this->id)?','.$this->id:''),
             'duration_type'=>'bail|sometimes|in:YEAR,MONTH',
             'min_age'=>'bail|'.($id?'sometimes':'required').'|min:0,max:100',
-            'max_age'=>'bail|'.($id?'sometimes':'required').'|min:1',
+            'max_age'=>'bail|sometimes|nullable|min:1',
             'status'=>'bail|sometimes|in:ACTIVE,INACTIVE'
         ];
    }
