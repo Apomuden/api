0a17fa241bcfd52c06932bdfe9868dcf44dc4e49
Clement Sam <clementsam75@gmail.com>
2020-01-11 11:26:32 +0000

    Added validation to check whether the subcategory id provided for clinics is a subcategory under the CONSULTATION SERVICE

--- app/Http/Requests/Clinic/ConsultationRequest.php
+++ app/Http/Requests/Clinic/ConsultationRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Clinic;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\ApiFormRequest;
 
-class ConsultationRequest extends FormRequest
+class ConsultationRequest extends ApiFormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -23,8 +23,16 @@ class ConsultationRequest extends FormRequest
      */
     public function rules()
     {
+        $id = $this->route('consultation') ?? null;
         return [
-            //
+            'user_id'=>'bail|sometimes|integer|exists:users,id',
+            'clinic_id'=>'bail|'.($id ? 'sometimes' : 'required').'|integer|exists:clinics,id',
+            'patient_id'=>'bail|'.($id ? 'sometimes' : 'required').'|integer|exists:patients,id',
+            'service_price_id'=>'bail|sometimes|integer|exists:service_prices,id',
+            'scheduled_for' => 'bail|sometimes|date',
+            'started_at' => 'bail|sometimes|date',
+            'ended_at' => 'bail|sometimes|date',
+            'status'=>'bail|sometimes|in:ACTIVE,INACTIVE'
         ];
     }
 }
