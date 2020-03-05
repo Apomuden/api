2feb8ebdffddf17738d9965eb13c67f2dc6dbaf7
Andrew Sam <i4real@zoho.com>
2020-03-05 07:57:46 +0000

    Taken off consultant id from attendance payload

--- app/Http/Controllers/Registration/ConsultationController.php
+++ app/Http/Controllers/Registration/ConsultationController.php
@@ -85,7 +85,6 @@ class ConsultationController extends Controller
         $message = $this->routeName === 'consultationservicerequests.store' ? 'Consultation request created' : 'Consultation Service created';
         $response = $this->repository->store($request->all());
 
-
         return  ApiResponse::withOk( $message, new ConsultationResource($response->refresh()));
     }
 
