2d319a506f59641e75076588d85c25a429b3abe8
Clement Sam <clementsam75@gmail.com>
2020-03-12 01:06:40 +0000

    Split age duratio_type into min_age_unit and max_age_unit

--- app/Http/Resources/AgeGroupResource.php
+++ app/Http/Resources/AgeGroupResource.php
@@ -19,9 +19,11 @@ class AgeGroupResource extends JsonResource
             return [
                 'id'=>$this->id,
                 'name'=>$this->name,
-                'duration_type'=>$this->duration_type,
+                //'duration_type'=>$this->duration_type,
                 'min_age'=>$this->min_age,
                 'max_age'=>$this->max_age,
+                'min_age_unit'=>$this->min_age_unit,
+                'max_age_unit'=>$this->max_age_unit,
                 'status'=>$this->status,
                 'created_at'=>DateHelper::toDisplayDateTime($this->created_at),
                 'updated_at'=>DateHelper::toDisplayDateTime($this->updated_at)
