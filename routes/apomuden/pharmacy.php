a7e4ec62d8e946e38c1f8c0677f9808786463056
Clement Sam <clementsam75@gmail.com>
2020-04-22 12:17:55 +0000

    Updated pharmacy routes

--- routes/apomuden/pharmacy.php
+++ routes/apomuden/pharmacy.php
@@ -4,37 +4,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('products','Pharmacy\ProductsController',[
     //'only'=>['index','show','store','update'],
-    'module'=>'acct-mgt',
-    'component'=>'receipt'
+    'module'=>null,
+    'component'=>'products'
 ]);
 
 Route::apiResource('producttypes','Pharmacy\ProductTypeController',[
     //'only'=>['index','show','store','update'],
-    'module'=>'acct-mgt',
-    'component'=>'receipt'
+    'module'=>null,
+    'component'=>'producttypes'
 ]);
 
 Route::apiResource('productcategories','Pharmacy\ProductCategoryController',[
     //'only'=>['index','show','store','update'],
-    'module'=>'acct-mgt',
-    'component'=>'receipt'
+    'module'=>null,
+    'component'=>'productcategories'
 ]);
 
 Route::apiResource('productforms','Pharmacy\ProductFormController',[
     //'only'=>['index','show','store','update'],
-    'module'=>'acct-mgt',
-    'component'=>'receipt'
+    'module'=>null,
+    'component'=>'productforms'
 ]);
 
 Route::apiResource('productformunits','Pharmacy\ProductFormUnitController',[
     //'only'=>['index','show','store','update'],
-    'module'=>'acct-mgt',
-    'component'=>'receipt'
+    'module'=>null,
+    'component'=>'productformunits'
 ]);
 
 Route::apiResource('medicineroutes','Pharmacy\MedicineRouteController',[
     //'only'=>['index','show','store','update'],
-    'module'=>'acct-mgt',
-    'component'=>'receipt'
+    'module'=>null,
+    'component'=>'medicineroutes'
 ]);
 
