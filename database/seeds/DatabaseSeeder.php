1d9399e31ae92f5300b1b8b8ef42949f5c085665
Andrew Sam <i4real@zoho.com>
2020-04-28 14:33:16 +0000

    Updated Database Seeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(RolesTableSeeder::class);
         //$this->call(ModuleTableSeeder::class);
         $this->call(ComponentsTableSeeder::class);
-        //$this->call(PermissionTableSeeder::class);
+        $this->call(PermissionTableSeeder::class);
         // $this->call(CompanySeeder::class);
         // $this->call(AgeGroupTableSeeder::class);
         //$this->call(AgeClassificationTableSeeder::class);
