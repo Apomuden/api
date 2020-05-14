ce89cee27cb4cb2b03e4761dd50c1ff5e4510209
Wilfred Asomani <devwilfredagyei@gmail.com>
2020-05-14 20:36:44 +0000

    resource for grouping responses by consultation

---
+++ app/Http/Resources/Registrations/ConsultationGroupedQuestionResponseResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\Registrations;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property mixed patient
+ * @property mixed id
+ * @property mixed consultation_id
+ * @property mixed consultant_id
+ * @property mixed consultation_date
+ * @property mixed responses
+ * @property mixed attendance_date
+ */
+class ConsultationGroupedQuestionResponseResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        $responses = ConsultationQuestionResponseResource::collection($this->responses);
+        $consultant = 'Unknown';
+        if (count($responses) > 0)
+            $consultant = $responses->resource[0]->toArray($responses->resource[0])['consultant_name'];
+        return [
+            'consultation_id' => $this->id,
+            'consultation_date' => $this->attendance_date,
+            'consultant_name' => $consultant,
+            'responses' => $responses
+        ];
+    }
+}
