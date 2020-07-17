96f0561f4cdbbc3884a26f85f4022c5d28a4bc6e
Clement Saam <clementsam75@gmail.com>
2020-07-17 19:32:30 +0000

    Added seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,9 +21,9 @@ class DatabaseSeeder extends Seeder
         // $this->call(RegionsTableSeeder::class);
         // $this->call(HospitalTableSeeder::class);
         // $this->call(RolesTableSeeder::class);
-        //$this->call(ModuleTableSeeder::class);
-        //$this->call(ComponentsTableSeeder::class);
-        //$this->call(PermissionTableSeeder::class);
+        $this->call(ModuleTableSeeder::class);
+        $this->call(ComponentsTableSeeder::class);
+        $this->call(PermissionTableSeeder::class);
         // $this->call(CompanySeeder::class);
         // $this->call(AgeGroupTableSeeder::class);
         //$this->call(AgeClassificationTableSeeder::class);
