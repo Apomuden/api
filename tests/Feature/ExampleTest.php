00a27b61337e1b928b4a8f7476439a085b6688f1
Clement Sam <clementsam75@gmail.com>
2020-07-18 15:25:12 +0000

    Resolve "CI/CD should update the server"

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,10 +12,18 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
+    /*
     public function testBasicTest()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+    */
+    public function testAuthLogin()
+    {
+        $response = $this->post('/v1/auth/login',['username'=>'wrong_username','password'=>'secret']);
+
+        $response->assertUnauthorized();
     }
 }
